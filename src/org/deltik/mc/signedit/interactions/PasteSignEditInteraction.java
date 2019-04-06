900b7318a3809a2f3619314e506e85576b84788e
Deltik <deltik@gmx.com>
2019-04-06 14:24:18 -0500

    Fixed a null PasteSignEditInteraction.getName()

--- src/org/deltik/mc/signedit/interactions/PasteSignEditInteraction.java
+++ src/org/deltik/mc/signedit/interactions/PasteSignEditInteraction.java
@@ -46,6 +46,6 @@ public class PasteSignEditInteraction implements SignEditInteraction {
 
     @Override
     public String getName() {
-        return null;
+        return "Paste lines in clipboard";
     }
 }
