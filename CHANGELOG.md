8836dfbf7b22449715b8f6b62b117d92d3501c52
Nick Liu <deltik@gmx.com>
2020-01-26 22:34:00 +0100

    Applied GPL-3 to all source code files
    Also bumped version from 1.10.3-SNAPSHOT to 1.11.0-SNAPSHOT

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## v1.10.3-SNAPSHOT (UNRELEASED)
+## v1.11.0-SNAPSHOT (UNRELEASED)
 
 ### Added
 
@@ -25,6 +25,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 * The configuration file `config.yml` is now rendered from a template.  The template includes comments to make configuration easier to understand without consulting the online documentation.
+
+### Under the Hood
+
+* Each source code file now contains the proper copyright and license notice.
 
 ## v1.10.2 (2019-04-11)
 
--- src/org/deltik/mc/signedit/exceptions/NullClipboardException.java
+++ src/org/deltik/mc/signedit/exceptions/NullClipboardException.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2017-2020 Deltik <https://www.deltik.org/>
+ *
+ * This file is part of SignEdit for Bukkit.
+ *
+ * SignEdit for Bukkit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SignEdit for Bukkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SignEdit for Bukkit.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.deltik.mc.signedit.exceptions;
 
 public class NullClipboardException extends NullPointerException {
